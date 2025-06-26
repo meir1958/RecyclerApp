690ead4740c0c7d5720ee6403527e474660a7457
meir_dahane <meirdahane@gmail.com>
2025-06-26 20:12:20 +0300

    Second Update

--- app/src/main/java/com/example/recyclerapp/MainActivity.java
+++ app/src/main/java/com/example/recyclerapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.recyclerapp;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +26,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -46,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i("lifecycle","MainActivity onCreate");
         initViews();
         showFrruits();
     }
@@ -55,7 +67,8 @@ public class MainActivity extends AppCompatActivity {
         actionBar = getSupportActionBar();
         rvFruits = findViewById(R.id.rvFruits);
         ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(
-                ItemTouchHelper.RIGHT,ItemTouchHelper.LEFT
+                ItemTouchHelper.UP | ItemTouchHelper.DOWN,
+                ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT
         ) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
@@ -86,7 +99,13 @@ public class MainActivity extends AppCompatActivity {
                     //arrayList.remove(position);
                     //adapter.notifyItemRemoved(position);
 
-                    alertRemove();
+                    moveLeft();
+                }
+                else if(direction == ItemTouchHelper.RIGHT)
+                {
+                    //Toast.makeText(MainActivity.this,  "RIGHT", Toast.LENGTH_SHORT).show();
+                    adapter.notifyItemChanged(position);
+                    moveRight(fruitItem);
                 }
             }
         };
@@ -172,12 +191,94 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         dialog.show();
 
     }
+    public void moveRight(FruitItem fruitItem)
+    {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_choose);
+        dialog.setCancelable(false);
+        TextView tvName = dialog.findViewById(R.id.tvName);
+        TextView tvDesc = dialog.findViewById(R.id.tvDesc);
+        ImageView ivPhoto = dialog.findViewById(R.id.ivPhoto);
+        ivPhoto.setImageResource(fruitItem.getFruitItemImage());
+        ivPhoto.setTag("" + fruitItem.getFruitItemImage());
 
-    public void alertRemove()
+        tvName.setText(fruitItem.getFruitItemTitle());
+        tvDesc.setText(fruitItem.getFruitItemDesc());
+        Button btCancel = dialog.findViewById(R.id.btCancel);
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.hide();
+            }
+        });
+        Spinner spinner = dialog.findViewById(R.id.spinner);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("בחר אפשרות");
+        list.add("שתף עם חבר");
+        list.add("חייג מספר");
+        list.add("חפש בגוגל");
+        list.add("פתח מפה");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+
+                Object x = parent.getItemAtPosition(position);
+                String value = x.toString();
+                if (value.equals("בחר אפשרות")) {
+                    Toast.makeText(MainActivity.this,"בחר אפשרות", Toast.LENGTH_SHORT).show();
+                }
+                else if (value.equals("שתף עם חבר"))
+                {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_TEXT, " שתף מידע " + fruitItem.getFruitItemTitle());
+                    Intent shareIntent = Intent.createChooser(intent, null);
+                    startActivity(shareIntent);
+                }
+                else if (value.equals("חייג מספר"))
+                {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                        return;
+                    }
+                    else {
+                        String phoneNum = "05227248904";
+                        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNum));
+                        startActivity(intent);
+                    }
+                }
+                else if (value.equals("חפש בגוגל"))
+                {
+                    String query = fruitItem.getFruitItemTitle();
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.google.com/search?q=" +  Uri.encode(query)));
+                    startActivity(intent);
+                }
+                else if (value.equals("פתח מפה"))
+                {
+                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                            Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"));
+                    startActivity(intent);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        dialog.show();
+    }
+
+    public void moveLeft()
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);// מופע של בילדר
         builder.setTitle("מחיקת פרי");
@@ -239,5 +340,33 @@ public class MainActivity extends AppCompatActivity {
         return arrayList;
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("lifecycle","MainActivity onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("lifecycle","MainActivity onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("lifecycle","MainActivity onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("lifecycle","MainActivity onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("lifecycle","MainActivity onDestroy");
+    }
 }
